2d70e5b51a1a1841750f0ee214d5ab11b9c09df3
Muhammad Daimus Suudi <daimus.suudi@gmail.com>
2023-01-06 14:22:22 +0700

    refactor: remove unnecessary class

--- src/components/ToDoListItem.js
+++ src/components/ToDoListItem.js
@@ -10,7 +10,7 @@ const ToDoListItem = (props) => {
                       defaultChecked={checked}
                       id="checked-checkbox"
                       type="checkbox"
-                      className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                      className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500"
                       onChange={(event) => props.handleCheck(id, event.target.checked)}
                     />
                   </div>
@@ -19,7 +19,7 @@ const ToDoListItem = (props) => {
                 <div>
                   <button
                     type="button"
-                    className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center mr-2"
                     onClick={() => props.handleDelete(id)}
                   >
                     <svg
